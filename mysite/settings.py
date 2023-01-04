6473e86e40f9adaf6647a16960fb87c686e69561
khoaminion <minionsdt@gmail.com>
2023-01-05 00:05:07 +0700

    lol

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'dashboard',
-    'django.contrib.staticfiles',
     'whitenoise.runserver_nostatic',
 ]
 
