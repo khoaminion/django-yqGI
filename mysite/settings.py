c3967ad9bafebce317d7049152ba963ad920ace8
khoaminion <minionsdt@gmail.com>
2023-01-05 00:26:08 +0700

    lol1

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'django-insecure-rc^*w^w&6g9_(uvx#6s*bnt!w)l0rdi%!l7mv#y%uc&x%wo5pk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 STATIC_URL = True
